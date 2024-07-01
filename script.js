26cf7770342c58c382ba05748d1def1b5302b42b
Oliver Ellison, MS SD <89275354+aurelius-in@users.noreply.github.com>
2024-07-01 14:01:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -35,46 +35,6 @@ document.addEventListener('DOMContentLoaded', function() {
             .catch(error => console.error(`Error fetching articles for ${category}:`, error));
     }
 
-    function loadCovers() {
-        fetch('assets/covers/thumbs/issues.json')
-            .then(response => response.json())
-            .then(covers => {
-                const container = document.getElementById('covers-container');
-                container.innerHTML = '';  // Clear any existing covers
-                covers.slice(0, 12).forEach(cover => {
-                    const issue = cover.replace('-', '').split('.')[0]; // Remove '-' and extension
-                    const coverDiv = document.createElement('div');
-                    coverDiv.classList.add('cover');
-                    coverDiv.innerHTML = `<img src="assets/covers/thumbs/${cover}" alt="${cover}">`;
-                    coverDiv.addEventListener('click', () => {
-                        window.location.href = `issue.html?issue=${issue}`;
-                    });
-                    container.appendChild(coverDiv);
-                });
-                if (covers.length > 12) {
-                    const viewMoreButton = document.createElement('button');
-                    viewMoreButton.classList.add('view-more-button');
-                    viewMoreButton.innerText = 'View More';
-                    viewMoreButton.addEventListener('click', () => {
-                        container.innerHTML = '';
-                        covers.forEach(cover => {
-                            const issue = cover.replace('-', '').split('.')[0]; // Remove '-' and extension
-                            const coverDiv = document.createElement('div');
-                            coverDiv.classList.add('cover');
-                            coverDiv.innerHTML = `<img src="assets/covers/thumbs/${cover}" alt="${cover}">`;
-                            coverDiv.addEventListener('click', () => {
-                                window.location.href = `issue.html?issue=${issue}`;
-                            });
-                            container.appendChild(coverDiv);
-                        });
-                        viewMoreButton.remove();
-                    });
-                    container.appendChild(viewMoreButton);
-                }
-            })
-            .catch(error => console.error('Error fetching covers:', error));
-    }
-
     function loadLatestIssue() {
         categories.forEach(category => {
             fetchArticles(category);
@@ -89,6 +49,4 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     loadLatestIssue();
-
-    document.getElementById('archives').addEventListener('click', () => {
-        document.getElementById('categories-container').style.display = '
+});
